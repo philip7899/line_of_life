5d688c960bb43e61afbdfe07cb251a1a8e38400d
JP <bonnerjp@gmail.com>
2014-04-13 20:15:11 -0400

    ported static landing assets (built) to index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ LineOfLife::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'pictures#new'
+   root 'static#index'
    resources :pictures
 
   # Example of regular route:
